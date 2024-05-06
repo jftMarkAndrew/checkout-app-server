28e2bf21a46adb176a62b37a78066586463b5363
Mark Andrew <jftmain@gmail.com>
2024-05-06 11:06:56 +0300

    config http

--- src/checkout/checkout.module.ts
+++ src/checkout/checkout.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { CheckoutService } from './checkout.service';
 import { CheckoutController } from './checkout.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [HttpModule, ConfigModule],
   providers: [CheckoutService],
   controllers: [CheckoutController],
 })
