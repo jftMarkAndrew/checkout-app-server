bc91664ca13904d3adf739fcf5e37f5d8e599bbe
Mark Andrew <jftmain@gmail.com>
2024-05-06 11:06:47 +0300

    config http

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,20 @@ import { AppService } from './app.service';
 import { CheckoutService } from './checkout/checkout.service';
 import { CheckoutController } from './checkout/checkout.controller';
 import { CheckoutModule } from './checkout/checkout.module';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [CheckoutModule],
+  imports: [
+    CheckoutModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+  ],
   controllers: [AppController, CheckoutController],
   providers: [AppService, CheckoutService],
 })
