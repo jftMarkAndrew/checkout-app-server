c2071ed45679d3429d2f6f4007f14618d9a0a2e3
Mark Andrew <jftmain@gmail.com>
2024-05-06 04:23:16 +0300

    checkout module controller service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CheckoutService } from './checkout/checkout.service';
+import { CheckoutController } from './checkout/checkout.controller';
+import { CheckoutModule } from './checkout/checkout.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CheckoutModule],
+  controllers: [AppController, CheckoutController],
+  providers: [AppService, CheckoutService],
 })
 export class AppModule {}
