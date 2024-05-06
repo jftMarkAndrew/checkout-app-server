be45b2523d639f79fa942ee71ab78a345922853b
Mark Andrew <jftmain@gmail.com>
2024-05-06 11:07:25 +0300

    session token

--- src/checkout/checkout.controller.ts
+++ src/checkout/checkout.controller.ts
@@ -1,12 +1,25 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller, Post, Res, HttpStatus } from '@nestjs/common';
 import { CheckoutService } from './checkout.service';
+import { Response } from 'express';
 
 @Controller('checkout')
 export class CheckoutController {
-  constructor(private readonly checkoutService: CheckoutService) {}
+  constructor(private checkoutService: CheckoutService) {}
 
-  @Post()
-  create(@Body() key: string) {
-    return this.checkoutService.checkout(key);
+  @Post('session-token')
+  async getSessionToken(@Res() res: Response) {
+    try {
+      const sessionData = await this.checkoutService.createCheckoutSession();
+      if (!sessionData.sessionToken) {
+        return res
+          .status(HttpStatus.BAD_REQUEST)
+          .json({ message: 'Failed to retrieve session token' });
+      }
+      return res.json({ sessionToken: sessionData.sessionToken });
+    } catch (error) {
+      return res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ message: 'Internal server error' });
+    }
   }
 }
--- src/checkout/checkout.service.ts
+++ src/checkout/checkout.service.ts
@@ -1,8 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Injectable()
 export class CheckoutService {
-  checkout(key: string) {
-    if (key) console.log('Key is here.');
+  constructor(private httpService: HttpService) {}
+
+  async createCheckoutSession(): Promise<any> {
+    const url = 'https://sandbox.unipaas.com/platform/pay-ins/checkout';
+    const headers = {
+      Authorization: `Bearer ${process.env.UNIPAAS_SANDBOX_PRIVATE_API_KEY}`,
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    };
+    const body = {
+      amount: 1000,
+      currency: 'GBP',
+      country: 'GB',
+      disablePaymentMethods: { disableCard: false, disablePayByBank: false },
+      email: 'jftmain@gmail.com',
+    };
+
+    try {
+      const response = await firstValueFrom(
+        this.httpService.post(url, body, { headers }),
+      );
+      return response.data;
+    } catch (error) {
+      console.error('Error when calling Unipaas:');
+      throw new HttpException(
+        'Failed to create checkout session',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
