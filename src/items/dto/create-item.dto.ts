af53eee7510c9242370c9700b52acc79df91249e
Mark Andrew <jftmain@gmail.com>
2024-05-18 20:15:41 +0300

    dto create item

---
+++ src/items/dto/create-item.dto.ts
@@ -0,0 +1,42 @@
+import { IsString, IsNumber, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateItemDto {
+  @ApiProperty()
+  @IsString()
+  name: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @ApiProperty()
+  @IsNumber()
+  amount: number;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsNumber()
+  quantity?: number;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsNumber()
+  fundingAmount?: number;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsNumber()
+  platformFee?: number;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  vendorId?: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  imageUrl?: string;
+}
