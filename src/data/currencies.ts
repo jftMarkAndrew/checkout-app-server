b97fc3bd318941abec785d108f69c993b38652a8
Mark Andrew <jftmain@gmail.com>
2024-05-18 22:26:23 +0300

    currencies

---
+++ src/data/currencies.ts
@@ -0,0 +1,7 @@
+import { Currency, CurrencyCode } from 'src/interfaces/currency.interface';
+
+export const currencies: Currency[] = [
+  { code: CurrencyCode.GBP },
+  { code: CurrencyCode.USD },
+  { code: CurrencyCode.EUR },
+];
---
+++ src/interfaces/currency.interface.ts
@@ -0,0 +1,9 @@
+export enum CurrencyCode {
+  GBP = 'GBP',
+  USD = 'USD',
+  EUR = 'EUR',
+}
+
+export interface Currency {
+  code: string;
+}
