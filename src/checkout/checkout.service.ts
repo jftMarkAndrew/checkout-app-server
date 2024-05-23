712635b2ec86e0b4b091e5cada43bf9592470a0d
Mark Andrew <jftmain@gmail.com>
2024-05-23 03:27:48 +0300

    refactor after the advice

--- src/checkout/checkout.service.ts
+++ src/checkout/checkout.service.ts
@@ -10,7 +10,8 @@ dotenv.config();
 export class CheckoutService {
   constructor(private httpService: HttpService) {}
 
-  async createCheckoutSession(
+  //todo: refactor
+  /* async createCheckoutSession(
     createCheckoutSessionDto: CreateCheckoutSessionDto,
   ): Promise<any> {
     const url = `${process.env.UNIPAAS_SANDBOX_URL}/pay-ins/checkout`;
@@ -28,6 +29,38 @@ export class CheckoutService {
       return response.data;
     } catch (error) {
       console.error('Error when calling Unipaas:');
+      throw new HttpException(
+        'Failed to create checkout session',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  } */
+
+  async createCheckoutSession(
+    createCheckoutSessionDto: CreateCheckoutSessionDto,
+  ): Promise<any> {
+    const url = `${process.env.UNIPAAS_SANDBOX_URL}/pay-ins/checkout`;
+    const headers = {
+      Authorization: `Bearer ${process.env.UNIPAAS_SANDBOX_PRIVATE_API_KEY}`,
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    };
+
+    return this.performHttpRequest(url, createCheckoutSessionDto, headers);
+  }
+
+  private async performHttpRequest(
+    url: string,
+    data: any,
+    headers: any,
+  ): Promise<any> {
+    try {
+      const response = await firstValueFrom(
+        this.httpService.post(url, data, { headers }),
+      );
+      return response.data;
+    } catch (error) {
+      console.error('Error when calling Unipaas:', error);
       throw new HttpException(
         'Failed to create checkout session',
         HttpStatus.INTERNAL_SERVER_ERROR,
